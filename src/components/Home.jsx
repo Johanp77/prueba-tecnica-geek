127b4bfa5efd1d9edef5118488f26f70fb654b61
Johan <johanp877@gmail.com>
2022-05-24 02:42:35 -0500

    Maquetado inicial para recibir datos de firestore

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { logoutAsync } from '../redux/actions/loginActions'
+import TableProducts from './TableProducts'
 
 const Home = () => {
     const dispatch = useDispatch()
   return (
     <div>
-        <h1>Home</h1>
         <div className="cabecera">
-            
+            <p>INGREDIENTES</p>
+            <h1>Nombre del plato</h1>
+            <span>Seleccionar todo</span> <span>|</span> <span>Deseleccionar todo</span>
+        </div>
+        <div className="contenedor-ingredientes">
+            <TableProducts/>
+        </div>
+        <div className="Resumen-factura">
+            <p>Items:</p>
+            <p>Subtotal</p>
+            <p>Gastos de envío</p>
+            <p>Total:</p>
+            <button>Comprar Ingredientes: </button>
         </div>
         <button onClick={() =>dispatch(logoutAsync())}>Cerrar Sesión</button>
 
---
+++ src/components/TableProducts.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import { doc, getDoc } from "firebase/firestore";
+
+const TableProducts = () => {
+
+    const [products, setProducts] = useState([])
+
+
+  return (
+    <div>TableProducts</div>
+  )
+}
+
+export default TableProducts
